2a83fb2f8a6c797e1b366a6357a66bb750b500b8
Hocine Zared <votre.email@exemple.com>
2026-02-21 17:44:37 +0100

    Test de la fonction calculer_distance avec les coordonnée du fichier points.txt (fonction lire_coordonnee) pour voir si les deux fonctions communiquent bien ensemble

--- phase1/main.py
+++ phase1/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np      
 import matplotlib as  mpl 
+import math 
 
 def lire_coordonnees(nom_fichier):
     """Lit un fichier texte et retourne une liste de points."""
@@ -16,3 +17,25 @@ def lire_coordonnees(nom_fichier):
     return points
 
 print(lire_coordonnees("phase1/points.txt"))
+
+mes_points = lire_coordonnees("phase1/points.txt") 
+
+def calculer_distance(point1, point2):
+    x1 = point1[0]
+    y1 = point1[1]
+    x2 = point2[0]
+    y2 = point2[1]
+    
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2) # On calcule la distance Euclidienne point 1 et point 2 
+    
+    return distance
+
+print(calculer_distance((0, 0), (3, 4)))
+
+point_A = mes_points[0]
+point_B = mes_points[1]
+
+test_avec_coordonnee_fichier = calculer_distance(point_A, point_B)
+print("Le point A est :", point_A)
+print("Le point B est :", point_B)
+print("La distance entre les deux est :", test_avec_coordonnee_fichier)
